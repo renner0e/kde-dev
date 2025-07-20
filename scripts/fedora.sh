7b21484d2b19568743ac7575c712b2a15725c945
renner <renner0@posteo.de>
2025-07-20 18:52:43 +0200

    feat: add starship

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -4,10 +4,11 @@
 dnf upgrade -y
 grep -v '^#' ./fedora.packages | xargs dnf install -y
 
+dnf copr enable -y atim/starship
 
 # This sets up the actual kde dev environment
 
-dnf install -y git python3-dbus python3-pyyaml python3-setproctitle
+dnf install -y git python3-dbus python3-pyyaml python3-setproctitle starship
 git clone https://invent.kde.org/sdk/kde-builder.git /usr/share/kde-builder
 ln -sf /usr/share/kde-builder/kde-builder /usr/bin/
 cd /usr/share/kde-builder
