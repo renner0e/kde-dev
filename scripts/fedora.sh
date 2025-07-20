671a9aa965c629f05967835f0a6f30acc31d8ff1
renner <renner0@posteo.de>
2025-07-20 18:32:39 +0200

    feat: add new stuff + rename

---
+++ scripts/fedora.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Update the container and install packages
+dnf upgrade -y
+grep -v '^#' ./fedora.packages | xargs dnf install -y
+
+
+# This sets up the actual kde dev environment
+set -eoux pipefail
+
+dnf install -y git python3-dbus python3-pyyaml python3-setproctitle
+git clone https://invent.kde.org/sdk/kde-builder.git /usr/share/kde-builder
+ln -sf /usr/share/kde-builder/kde-builder /usr/bin/
+cd /usr/share/kde-builder
+pip install pipenv
+
+yes | kde-builder --install-distro-packages
