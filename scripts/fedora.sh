6a588e62a5ac8e7d8bde3605bd7dcf0410090d12
renner <renner0@posteo.de>
2025-07-20 18:50:01 +0200

    fix: stupid error

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -6,7 +6,6 @@ grep -v '^#' ./fedora.packages | xargs dnf install -y
 
 
 # This sets up the actual kde dev environment
-set -eoux pipefail
 
 dnf install -y git python3-dbus python3-pyyaml python3-setproctitle
 git clone https://invent.kde.org/sdk/kde-builder.git /usr/share/kde-builder
